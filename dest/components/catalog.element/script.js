3a3e0814824d7c619c95a06c1b23abdd75f2fa17
Tatiana <djembuka@gmail.com>
2017-08-11 18:57:48 +0300

    New estate page

--- dest/components/catalog.element/script.js
+++ dest/components/catalog.element/script.js
@@ -8,5 +8,17 @@
             $.scrollTo(".b-estate__map", 500);
             e.preventDefault();
         });
+        $(window).scroll(function() {
+            if (!$(".b-estate-progressbar__percent").hasClass("i-animated") && $(document).scrollTop() + $(window).height() - $(".b-estate-progressbar").offset().top > 100) {
+                $(".b-estate-progressbar__percent").addClass("i-animated").css({
+                    width: "70%"
+                });
+            }
+        });
+        $(".b-top-menu a").click(function(e) {
+            $.scrollTo($("" + $(this).attr("href")), 500);
+            e.preventDefault();
+            return false;
+        });
     });
 })(jQuery);
