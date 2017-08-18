a1bb4a0708263222f8d231f674715b13982d119b
Tatiana <djembuka@gmail.com>
2017-08-18 13:36:29 +0300

    Footer fixes

--- dest/template/jscript.js
+++ dest/template/jscript.js
@@ -29,6 +29,27 @@
             }
         }
         windowEvents();
+        function sideBarCss() {
+            if (!$("#bx-panel").length) {
+                return;
+            }
+            $(".b-side-icons").css({
+                top: $("#bx-panel").height() + "px"
+            });
+            $(".b-side-bar__content").css({
+                paddingTop: $("#bx-panel").height() + "px"
+            });
+            $("#bx-panel-expander, #bx-panel-hider").bind("click", function() {
+                setTimeout(function() {
+                    $(".b-side-icons").css({
+                        top: $("#bx-panel").height() + "px"
+                    });
+                    $(".b-side-bar__content").css({
+                        paddingTop: $("#bx-panel").height() + "px"
+                    });
+                }, 0);
+            });
+        }
         function sideBar() {
             var $bar = $("#sideBar");
             var $button = $("#sideBarButton");
@@ -77,6 +98,7 @@
                 }
             });
         }
+        sideBarCss();
         sideBarTopButton();
         sideBar();
     });
