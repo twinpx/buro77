5c7729c290dc3873d88502af23dc34230661c5b4
Tatiana <djembuka@gmail.com>
2017-08-30 11:14:46 +0300

    Fixes

--- dest/components/catalog.element/script.js
+++ dest/components/catalog.element/script.js
@@ -11,7 +11,7 @@
         $(window).scroll(function() {
             if (!$(".b-estate-progressbar__percent").hasClass("i-animated") && $(document).scrollTop() + $(window).height() - $(".b-estate-progressbar").offset().top > 100) {
                 $(".b-estate-progressbar__percent").addClass("i-animated").css({
-                    width: $(".b-estate-progressbar__percent").text() + "%"
+                    width: parseInt($(".b-estate-progressbar__percent").text(), 10) + "%"
                 });
             }
         });
