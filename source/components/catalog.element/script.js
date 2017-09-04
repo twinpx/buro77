17f30188bea5d3d2cad36aa55b7776033484320e
Tatiana <djembuka@gmail.com>
2017-09-04 12:40:56 +0300

    Conditions for the estate's progress bar

--- source/components/catalog.element/script.js
+++ source/components/catalog.element/script.js
@@ -17,12 +17,13 @@
       e.preventDefault();
     });
     
-    $( window ).scroll( function() {
-      
-      if ( !$( '.b-estate-progressbar__percent' ).hasClass( 'i-animated' ) && ($( document ).scrollTop() + $( window ).height()) - $( '.b-estate-progressbar' ).offset().top > 100 ) {
-        $( ".b-estate-progressbar__percent" ).addClass( 'i-animated' ).css({ width: parseInt($( '.b-estate-progressbar__percent' ).text(), 10) + '%' });
-      }
-    });
+    if ( document.getElementById( 'estateProgress' )) {
+      $( window ).scroll( function() {
+        if ( !$( '.b-estate-progressbar__percent' ).hasClass( 'i-animated' ) && ($( document ).scrollTop() + $( window ).height()) - $( '.b-estate-progressbar' ).offset().top > 100 ) {
+          $( ".b-estate-progressbar__percent" ).addClass( 'i-animated' ).css({ width: parseInt($( '.b-estate-progressbar__percent' ).text(), 10) + '%' });
+        }
+      });
+    }
     
     $( '.b-top-menu a' ).click( function(e) {
       $.scrollTo( $( '' + $( this ).attr( 'href' )), 500 );
