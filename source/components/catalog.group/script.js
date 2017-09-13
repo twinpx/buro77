e7e2680fe825e6a08da4e29bc312b8d2b5cae2a8
Tatiana <djembuka@gmail.com>
2017-09-13 15:16:43 +0300

    Fixes for group page

--- source/components/catalog.group/script.js
+++ source/components/catalog.group/script.js
@@ -17,6 +17,20 @@
       e.preventDefault();
     });
     
+    if ( document.getElementById( 'estateProgress' )) {
+      $( window ).scroll( function() {
+        if ( !$( '.b-estate-progressbar__percent' ).hasClass( 'i-animated' ) && ($( document ).scrollTop() + $( window ).height()) - $( '.b-estate-progressbar' ).offset().top > 100 ) {
+          $( ".b-estate-progressbar__percent" ).addClass( 'i-animated' ).css({ width: parseInt($( '.b-estate-progressbar__percent' ).text(), 10) + '%' });
+        }
+      });
+    }
+    
+    $( '.b-top-menu a' ).click( function(e) {
+      $.scrollTo( $( '' + $( this ).attr( 'href' )), 500 );
+      e.preventDefault();
+      return false;
+    });
+    
   });
 
 }( jQuery ));
