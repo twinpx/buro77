ca5686f14accad594d2b7ff6c622905c3bb52f57
djembuka <djembuka@gmail.com>
2020-07-22 17:30:04 +0300

    Search field fixed

--- dest/components/catalog.gallery/script.js
+++ dest/components/catalog.gallery/script.js
@@ -1,28 +1,11 @@
 (function($) {
     "use strict";
     $(function() {
-        $("#gallery").slideme({
-            arrows: true,
-            labels: {
-                next: "next",
-                prev: "prev"
-            },
-            loop: true,
-            transition: "slide",
-            resizable: {
-                width: 990,
-                height: 450
-            },
-            pagination: "numbers",
-            thumbs: {
-                width: 50,
-                height: 50
-            },
-            autoslide: true,
-            autoslidehoverstop: true,
-            interval: 2e3,
-            swipe: true,
-            touch: true
-        });
+        if (window.matchMedia("(max-width: 767px)").matches) {
+            $(".fotorama").data("fotorama").resize({
+                width: "200%",
+                height: "400px"
+            });
+        }
     });
 })(jQuery);
